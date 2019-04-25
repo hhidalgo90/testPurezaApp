4dc1a869ec568086d1701017cbf942fec62db492
hhidalgo90 <h.hidalgo1990@gmail.com>
2019-04-24 23:04:43 -0400

    se agrega funcionalidad para la camara

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav } from 'ionic-angular';
 import { NavController } from 'ionic-angular';
 import {MostrarPreguntasPage} from '../mostrar-preguntas/mostrar-preguntas.page';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -10,7 +11,8 @@ import {MostrarPreguntasPage} from '../mostrar-preguntas/mostrar-preguntas.page'
 export class HomePage {
   @ViewChild('content') navCtrl: NavController;
   public mostrarPreguntas: any = MostrarPreguntasPage;
-  constructor() {
+  options : any;
+  constructor( private camera: Camera) {
 
   }
 
@@ -18,4 +20,24 @@ export class HomePage {
   public obtenerPreguntas() {
     this.navCtrl.setRoot(this.mostrarPreguntas);
   }
+
+    //Utilizar la camara del celu.
+    sacarFoto(): void{
+      console.log("Llegue a sacar foto");
+  
+      this.options = {
+        quality: 100,
+        destinationType: this.camera.DestinationType.FILE_URI,
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType: this.camera.MediaType.PICTURE,
+        saveToPhotoAlbum : true
+      }
+      this.camera.getPicture(this.options).then((imageData) => {
+        // imageData is either a base64 encoded string or a file URI
+        // If it's base64 (DATA_URL):
+        let base64Image = 'data:image/jpeg;base64,' + imageData;
+       }, (err) => {
+        console.log("Ocurrio un error: " + err);
+       });
+    }
 }
--- src/app/mostrar-preguntas/mostrar-preguntas.page.ts
+++ src/app/mostrar-preguntas/mostrar-preguntas.page.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ObtenerPreguntasService } from '../services/obtener-preguntas.service';
 import { Pregunta } from '../clases/pregunta';
-import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-mostrar-preguntas',
@@ -11,12 +10,10 @@ import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 export class MostrarPreguntasPage implements OnInit {
 
   preguntas : Pregunta[];
-  options : any;
-  constructor(private obtenerPreguntasService : ObtenerPreguntasService, private camera: Camera ) { }
+  constructor(private obtenerPreguntasService : ObtenerPreguntasService ) { }
 
   ngOnInit() {
     this.getPreguntas();
-    this.sacarFoto();
   }
 
   //Obtiene preguntas desde un servicio REST
@@ -27,25 +24,5 @@ export class MostrarPreguntasPage implements OnInit {
   guardarRespuestas(): void{
     console.log("llegue a guardarRespuestas: respuestas " + this.preguntas.values);
 }
-
-  //Obtiene preguntas desde un servicio REST
-  sacarFoto(): void{
-    console.log("Llegue a sacar foto");
-
-    this.options = {
-      quality: 100,
-      destinationType: this.camera.DestinationType.FILE_URI,
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
-    }
-    this.camera.getPicture(this.options).then((imageData) => {
-      // imageData is either a base64 encoded string or a file URI
-      // If it's base64 (DATA_URL):
-      let base64Image = 'data:image/jpeg;base64,' + imageData;
-     }, (err) => {
-      console.log("Ocurrio un error: " + err);
-     });
-  }
-
 
 }
