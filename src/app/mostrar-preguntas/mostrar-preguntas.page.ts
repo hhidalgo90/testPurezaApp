f6e840df85e859976ecc750d4c70bc8dd48a7021
hhidalgoe <h.hidalgo1990@gmail.com>
2019-06-04 15:38:27 -0400

    Se agrega slice para avanzar por las preguntas.

--- src/app/mostrar-preguntas/mostrar-preguntas.page.ts
+++ src/app/mostrar-preguntas/mostrar-preguntas.page.ts
@@ -12,19 +12,43 @@ export class MostrarPreguntasPage implements OnInit {
   preguntas : Pregunta[];
   unit ="";
   public titulo = "Test Pureza";
+  inicio : number;
+  tope : number;
+  numeroPreguntas : number;
+  mostrarBtnSiguiente : boolean;
+  mostrarBtnFinalizar : boolean;
+
   constructor(private obtenerPreguntasService : ObtenerPreguntasService ) { }
 
   ngOnInit() {
     this.getPreguntas();
+    this.inicio = 0;
+    this.tope = 5;
+    this.mostrarBtnSiguiente = true; 
+    this.mostrarBtnFinalizar = false;
   }
 
   //Obtiene preguntas desde un servicio REST
   getPreguntas(): void{
-    this.obtenerPreguntasService.getPreguntas().subscribe(preguntas=>this.preguntas = preguntas);
+    this.obtenerPreguntasService.getPreguntas().subscribe(resultado=>{
+      this.preguntas = resultado
+    });
   }
 
   guardarRespuestas(): void{
     console.log("llegue a guardarRespuestas: respuestas " + this.preguntas[0].respuesta);
 }
 
+siguientePregunta(){
+  this.numeroPreguntas = this.preguntas.length;
+  if(this.tope < this.numeroPreguntas){
+    this.inicio+=5;
+    this.tope+=5;
+  }
+  else{
+    console.log("Se acabo esta mierda");
+    this.mostrarBtnSiguiente = false;
+    this.mostrarBtnFinalizar = true;
+  }
+}
 }
