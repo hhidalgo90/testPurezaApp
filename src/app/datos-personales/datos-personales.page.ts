dd9196ac0a4370a9c5cbdffcd7f784ff0e913ff7
Hector Hidalgo <h.hidalgo1990@gmail.com>
2019-07-16 14:38:12 -0400

    Se modifica estilo de la app.

--- src/app/datos-personales/datos-personales.page.ts
+++ src/app/datos-personales/datos-personales.page.ts
@@ -16,19 +16,6 @@ public datosUsuario = new Usuario;
 mensajeError : String;
 formUser : FormGroup;
 
-validation_messages = {
-  'nombreUser': [
-      { type: 'required', message: 'Username is required.' },
-      { type: 'minlength', message: 'Username must be at least 5 characters long.' },
-      { type: 'maxlength', message: 'Username cannot be more than 25 characters long.' },
-      { type: 'pattern', message: 'Your username must contain only numbers and letters.' },
-      { type: 'validUsername', message: 'Your username has already been taken.' }
-    ],
-    'name': [
-      { type: 'required', message: 'Name is required.' }
-    ]
-  };
-
 sexos: any[] = [
   {
     id: 1,
